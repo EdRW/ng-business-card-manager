3dfb59b5f7bdacdff64db9e366b744b0c4434677
Edmund Wright <nekurasky@gmail.com>
2018-11-17 13:33:24 -0600

    created contacts m, contact-list c, contact-details c, add-new-contact c

---
+++ src/app/dashboard/contacts/contacts.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ContactsRoutingModule } from './contacts-routing.module';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactDetailsComponent } from './contact-details/contact-details.component';
+
+@NgModule({
+  declarations: [ContactListComponent, ContactDetailsComponent],
+  imports: [
+    CommonModule,
+    ContactsRoutingModule
+  ]
+})
+export class ContactsModule { }
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -4,10 +4,12 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 import { CoreModule } from '../core/core.module';
+import { AddNewContactComponent } from './add-new-contact/add-new-contact.component';
 
 @NgModule({
   declarations: [
     DashboardComponent,
+    AddNewContactComponent,
   ],
   imports: [
     CommonModule,
