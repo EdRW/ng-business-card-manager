3e561d62e60cd12fa438b6a0dea47a2690e53834
Edmund Wright <nekurasky@gmail.com>
2018-11-17 14:00:25 -0600

    added routes for dashboard, contacts, and add contact

--- src/app/dashboard/contacts/contacts-routing.module.ts
+++ src/app/dashboard/contacts/contacts-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactDetailsComponent } from './contact-details/contact-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: ContactListComponent
+}, {
+  path: 'details',
+  component: ContactDetailsComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
+import { AddNewContactComponent } from './add-new-contact/add-new-contact.component';
 
 const routes: Routes = [{
   path: '',
   component: DashboardComponent,
+  children: [{
+    path: 'contacts',
+    loadChildren: './contacts/contacts.module#ContactsModule'
+  }, {
+    path: 'addcontact',
+    component: AddNewContactComponent
+  },
+  { path: '', redirectTo: 'contacts', pathMatch: 'full' },
+  { path: '**', redirectTo: 'contacts' }]
 }];
 
 @NgModule({
