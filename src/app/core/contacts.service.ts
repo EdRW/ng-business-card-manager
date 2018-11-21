74a1df119d9f1ef05b4fb4e12f06099f7b4a5e72
Edmund Wright <nekurasky@gmail.com>
2018-11-20 23:15:58 -0600

    added a fullname search to prevent duplicate contacts

--- src/app/core/contacts.service.ts
+++ src/app/core/contacts.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
 import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
 import { BusinessCard } from '../shared/models/business-card';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { map, every, take, tap } from 'rxjs/operators';
 import { Contact } from '../shared/models/contact';
 
 @Injectable()
@@ -15,14 +15,14 @@ export class ContactsService {
     private db: AngularFireDatabase
     ) {
     console.log('CONTACT SERVICE CONSTRUCTOR CALLED');
-    this.authService.getUserStatus().subscribe( user => {
+    this.authService.getUserStatus()
+    .subscribe( user => {
         this.userContactsRef = this.db.list<BusinessCard>(`users/${user.uid}/contacts`);
         console.log(`NEW CONTACTS TO WILL BE WRITTEN TO: users/${user.uid}/contacts`);
     });
   }
 
   getUserContacts(): Observable<BusinessCard[]> {
-    // TODO find a way to make this available to comonents in their constructors
     return this.userContactsRef.valueChanges();
   }
 
@@ -36,16 +36,30 @@ export class ContactsService {
     );
   }
 
-  addNewContact(businessCard: BusinessCard): string {
-    console.log(`ADDING A NEW USER ${businessCard.email}`);
-    return this.userContactsRef.push(businessCard).key;
+  addNewContact(businessCard: BusinessCard) {
+    this.searchFullName(businessCard.firstName, businessCard.lastName).pipe(
+      take(1),
+    )
+    .subscribe( bool => {
+      if (!bool) {
+        console.log(`ADDING A NEW CONTACT ${businessCard.email}`);
+        // tslint:disable-next-line:no-unused-expression
+        this.userContactsRef.push(businessCard).key;
+      } else {
+        console.log(`CONTACT ALREADY EXISTS ${businessCard.email}`);
+      }
+    });
   }
 
   getContact(key: string): Observable<BusinessCard> {
     return this.db.object<BusinessCard>(`users/${this.authService.userUid}/contacts/${key}`).valueChanges();
   }
 
-  searchFullName(firstName: string, lastName: string) {
-    // return
+  searchFullName(firstName: string, lastName: string): Observable<boolean> {
+    return this.getUserContacts().pipe(
+      // tslint:disable-next-line:max-line-length
+      map( contacts => contacts.some( contact => contact.firstName.toLocaleLowerCase() === firstName.toLocaleLowerCase() && contact.lastName.toLocaleLowerCase() === lastName.toLocaleLowerCase())
+      ),
+    );
   }
 }
